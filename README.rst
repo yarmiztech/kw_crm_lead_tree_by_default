2814ce1b7fb65d3761d6e9d743089d49fe81e026
enzapps <snippebucket@gmail.com>
2022-10-14 08:10:03 +0530

    <yt>

---
+++ README.rst
@@ -0,0 +1,6 @@
+Default tree view for CRM lead
+===============================
+
+It change default CRM lead view to tree instead of kanban.
+
+This module is developed by the `KitWorks <https://kitworks.systems/>`__.
---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Default tree view for CRM lead',
+
+    'author': 'Kitworks Systems',
+    'website': 'https://kitworks.systems/',
+
+    'category': 'Productivity',
+    'license': 'OPL-1',
+    'version': '14.0.1.0.1',
+    'depends': [
+        'crm',
+    ],
+    'data': [
+        'views/crm_lead_views.xml',
+    ],
+    'installable': True,
+
+    'images': [
+        'static/description/cover.png',
+        'static/description/icon.png',
+    ],
+}
